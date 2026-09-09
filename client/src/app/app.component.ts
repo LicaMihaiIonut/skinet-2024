fb641b8fe4d10066813d18bd531310e3f4f37334
LicaMihaiIonut <licamihai26@yahoo.ro>
2026-09-09 10:23:51 +0300

    Section 9 at lesson 83

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,25 +5,16 @@ import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy } from '@angular/core';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.Eager
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/'
-  private http = inject(HttpClient);
+export class AppComponent {
   title = 'Skinet';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-    })
-  }
+  
 }
